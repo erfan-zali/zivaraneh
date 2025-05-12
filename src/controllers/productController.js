a0c1c83067a2d0b69ba98a25aa92b25c8eca80dc
erfan_zali <erfanzali.dev@gmail.com>
2025-05-12 17:18:22 +0330

    fix bugs

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -12,17 +12,24 @@ const getProducts = async (req, res) => {
 }
 
 const getProductByCategory = async (req, res) => {
-    try {
-        const { category } = req.params;
-        const products = await Product.find({ category: category });
-        if(!products.length) {
-            res.status(404).json({ message: 'No product found in this category' });
-        }
-        res.status(200).send(products);
-    } catch (error) {
-        res.status(500).send({ message: 'Server Error', error: error.message });
+  try {
+    const { category } = req.params;
+    const products = await Product.find({ category: category });
+
+    if (!products.length) {
+      return res
+        .status(404)
+        .json({ message: "No product found in this category" });
     }
-}
+
+    return res.status(200).json(products);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Server Error", error: error.message });
+  }
+};
+
 
 export {
   getProducts,
